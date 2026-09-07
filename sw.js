8d36e8f2068f17346b5723e40263c771b9a61e6e
magerante241-boop <magerante241@gmail.com>
2026-09-07 22:45:28 +0100

    Bump cache v51 (fix numpad persistant)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "magerante-v50"; // ⚠️ à incrémenter à CHAQUE nouvelle livraison
+const CACHE_NAME = "magerante-v51"; // ⚠️ à incrémenter à CHAQUE nouvelle livraison
 const CORE_ASSETS = [
   "./index.html",
   "./dashboard.html",
