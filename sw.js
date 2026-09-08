36417abdd39916a01baa6351ff6a49b8b5640eb0
magerante241-boop <magerante241@gmail.com>
2026-09-08 09:46:27 +0100

    Bump cache v58 (mode Facturier par défaut)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "magerante-v57"; // ⚠️ à incrémenter à CHAQUE nouvelle livraison
+const CACHE_NAME = "magerante-v58"; // ⚠️ à incrémenter à CHAQUE nouvelle livraison
 const CORE_ASSETS = [
   "./index.html",
   "./dashboard.html",
