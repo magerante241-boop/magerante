3a86c4252138b6ac6e619ce0f16e0a05134e4304
magerante241-boop <magerante241@gmail.com>
2026-09-08 10:07:57 +0100

    Bump cache v62 (nav compactée)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "magerante-v61"; // ⚠️ à incrémenter à CHAQUE nouvelle livraison
+const CACHE_NAME = "magerante-v62"; // ⚠️ à incrémenter à CHAQUE nouvelle livraison
 const CORE_ASSETS = [
   "./index.html",
   "./dashboard.html",
