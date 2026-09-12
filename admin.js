eaed901366ad6a85256bf90f16f535d81579a226
magerante241-boop <magerante241@gmail.com>
2026-09-12 10:46:45 +0100

    debug: verifier Chart.js charge

--- admin.js
+++ admin.js
@@ -427,7 +427,7 @@ async function ouvrirEditionEtablissement(id, etablissements) {
   }
 }
 
-function attendreChart(callback, tentatives = 0) { if (tentatives === 0) alert("Chart existe: " + !!window.Chart);
+function attendreChart(callback, tentatives = 0) { if (tentatives === 0) alert("Chart existe: " + !!window.Chart); if (tentatives === 0) alert("Chart existe: " + !!window.Chart);
   if (window.Chart) {
     callback();
     return;
