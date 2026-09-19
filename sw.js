df877d67db18f98002b52b97169e67ff5c77c4a5
magerante241-boop <magerante241@gmail.com>
2026-09-19 17:02:08 +0100

    Bump cache v205

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "magerante-v204"; // ⚠️ à incrémenter à CHAQUE nouvelle livraison
+const CACHE_NAME = "magerante-v205"; // ⚠️ à incrémenter à CHAQUE nouvelle livraison
 const CORE_ASSETS = [
   "./index.html",
   "./dashboard.html",
